140f37ce79bf6a75e0a87675e0a9648bf27180fb
GFite <geoff@fiteanalytics.com>
2021-04-04 18:16:11 -0700

    content

--- finx-io/src/utils/menu.js
+++ finx-io/src/utils/menu.js
@@ -3,98 +3,14 @@ const menu = [
     name: 'Ecosystem',
     sublinks: [
       {
-        name: 'FINX App',
-        link: 'https://app.finx.io/#/',
-        description: 'Corporate Debt Pools'
+        name: 'Vehicle Digitization',
+        link: 'https://vehicles.finx.io/#/',
+        description: 'Title, Registration, Insurance, Access and Payments'
       },
       {
-        name: 'FINX Analytics',
-        link: 'https://info.finx.io/',
-        description: 'FINX analytics and historical data'
-      },
-      {
-        name: 'Uniswap V2',
-        link: 'https://uniswap.org/',
-        description: 'Uniswap V2 DEX'
-      },
-      {
-        name: 'Token Lists',
-        link: 'https://tokenlists.org/',
-        description: 'A new Ethereum token list standard.'
-      },
-      {
-        name: 'Sybil',
-        link: 'https://sybil.org/#/',
-        description: 'Identities for governance'
-      }
-    ]
-  },
-  {
-    name: 'Token Sale',
-    sublinks: [
-      {
-        name: 'Genesis FINX Token Sale',
-        link: '/blog/finx'
-      }
-    ]
-  },
-  {
-    name: 'Documents',
-    sublinks: [
-      {
-        name: 'FINX Documents',
-        link: 'docs/v2/protocol-overview/how-finx-works'
-      },
-      { name: 'Whitepaper', link: '/docs/v2/whitepaper' },
-      { name: 'Uniswap V2 Whitepaper', link: '/whitepaper.pdf' }
-    ]
-  },
-  {
-    name: 'Developers',
-    sublinks: [
-      { name: 'GitHub', link: 'https://github.com/FinX-IO' },
-      { name: 'Developer Guide', link: '/docs/v2/sdk'},
-      { name: 'Bug Bounty', link: '/bug-bounty' }
-    ]
-  },
-  {
-    name: 'Governance',
-    sublinks: [
-      {
-        name: 'FINX Token',
-        link: '/blog/finx'
-      },
-      { name: 'Audit', link: '/audit.html' },
-      { name: 'Voting Portal', link: 'https://app.finx.io/#/vote' },
-      { name: 'Documentation', link: '/docs/v2/governance/overview' }
-    ]
-  },
-  {
-    name: 'Community',
-    sublinks: [
-      { name: 'FINX Discord', link: 'https://discord.gg/FCfyBSbCU5' },
-      { name: 'FINX Twitter', link: 'https://twitter.com/FinX_io' },
-      { name: 'FINX Reddit', link: 'https://www.reddit.com/r/FINX' }
-    ]
-  },
-  {
-    name: 'About',
-    sublinks: [
-      {
-        name: 'Blog',
-        link: '/blog'
-      },
-      {
-        name: 'Info',
-        link: '/about'
-      },
-      {
-        name: 'FAQ',
-        link: '/faq'
-      },
-      {
-        name: 'Jobs',
-        link: '/about#jobs'
+        name: 'Art Digitization',
+        link: 'https://art.finx.io/',
+        description: 'NFT your Fine Art Assets'
       }
     ]
   }
